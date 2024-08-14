23b0f0355133aa556be03b18d7585c7812f55cf1
MayankAgrawal94 <mayankagrawal4949@gmail.com>
2024-08-14 19:50:43 +0530

    Handle Two Numbers
    Test: Written a test for a string containing two numbers separated by a comma, and return their sum.
    Code: Modified the code to handle two numbers.
    
    Explaination:
    - Splitting the String: The input string is split by commas using the split(',') method, resulting in an array of string numbers.
    - Mapping to Numbers: The map(num => parseInt(num, 10)) function converts each string in the array to an integer.
    - Summing the Numbers: The reduce((sum, num) => sum + num, 0) method adds up all the numbers in the array and returns the sum.

--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -15,4 +15,8 @@ describe('StringCalculator', () => {
     it('should return the number for a single number input', () => {
         expect(calculator.add('1')).to.equal(1);
     });
+
+    it('should return the sum of two numbers', () => {
+        expect(calculator.add('1,2')).to.equal(3);
+    });
 })
